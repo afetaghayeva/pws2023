1a8a8a60764344cb7800e484f299504b6ba46778
afetaghayeva <afetagayeva.2003@gmail.com>
2024-02-23 18:38:48 +0100

    commit

--- task.js
+++ task.js
@@ -1,123 +1,136 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const schema = new mongoose.Schema({
+const schema = new mongoose.Schema(
+  {
     name: { type: String, required: true },
     project_id: { type: mongoose.ObjectId, required: true },
-    status: { type: Date, required: true, transform: v => v.toISOString().slice(0, 10) },
-    education: { type: Number, required: false, enum: [ 0, 1, 2 ], default: 0 },
-    projects: { type: [ mongoose.ObjectId ], required: false, default: [] }
-}, {
+    status: { type: Number, required: true, enum: [0, 1, 2, 3], default: 0 },
+    workers: { type: [mongoose.ObjectId], required: false, default: [] },
+  },
+  {
     versionKey: false,
-    additionalProperties: false
-})
+    additionalProperties: false,
+  }
+);
 
-let model = null
+let model = null;
 
 module.exports = {
+  getSchema: () => schema,
+  getModel: () => model,
 
-    getSchema: () => schema,
-    getModel: () => model,
+  init: (connection) => {
+    model = connection.model("Task", schema);
+    return model;
+  },
 
-    init: connection => {
-        model = connection.model('Person', schema)
-        return model
-    },
+  get: (req, res) => {
+    const _id = req.query._id;
+    if (_id) {
+      model
+        .findOne({ _id })
+        .then((data) => {
+          if (data) {
+            res.json(data);
+          } else {
+            res.status(404).json({ error: "No such object" });
+          }
+        })
+        .catch((err) => {
+          res.status(408).json({ error: err.message });
+        });
+    } else {
+      let aggregation = [
+        { $sort: { name: 1 } },
+        { $match: { name: { $regex: new RegExp(req.query.search, "i") } } },
+        {
+          $lookup: {
+            from: "projects",
+            localField: "project",
+            foreignField: "_id",
+            as: "project",
+          },
+        },
+        { $unwind: { path: "$project", preserveNullAndEmptyArrays: true } },
+        {
+          $lookup: {
+            from: "people",
+            localField: "workers",
+            foreignField: "_id",
+            as: "workers",
+          },
+        },
+      ];
+      if (req.query.project) {
+        aggregation.unshift({
+          $match: { project: new mongoose.Types.ObjectId(req.query.project) },
+        });
+      }
 
-    get: (req, res) => {
-        const _id = req.query._id
-        if(_id) {
-            model.findOne({ _id })
-            .then(data => {
-                if(data) {
-                    res.json(data)
-                } else {
-                    res.status(404).json({ error: 'No such object' })
-                }
-            })
-            .catch(err => {
-                res.status(408).json({ error: err.message })
-            })
-        } else {
-            let aggregation = [
-                { $sort: { lastName: 1, firstName: 1 }},
-                { $match: { $or: [
-                    { firstName: { $regex: new RegExp(req.query.search, 'i') } },
-                    { lastName: { $regex: new RegExp(req.query.search, 'i') } }
-                ]}}
-            ]
-            try {
-                let education = JSON.parse(req.query.education)
-                if(Array.isArray(education)) {
-                    aggregation.push({ $match: { education: { $in: education } } })    
-                }
-            } catch(err) {}
-            try {
-                let minProjectsCount = JSON.parse(req.query.minProjects)
-                if(minProjectsCount > 0) {
-                    aggregation.push({ $match: { projects: { $exists: true }}})
-                    aggregation.push({ $set: { projectsCount: { $size: '$projects' }}})
-                    aggregation.push({ $match: { projectsCount: { $gte: minProjectsCount }}})
-                    aggregation.push({ $sort: { projectsCount: -1 }})
-                }
-            } catch(ex) {}
-            aggregation.push({ $skip: parseInt(req.query.skip) || 0 })
-            aggregation.push({ $limit: parseInt(req.query.limit) || 10 })
-            aggregation.push({ $lookup: {
-                from: 'projects',
-                localField: 'projects',
-                foreignField: '_id',
-                as: 'projects',
-                pipeline: [
-                    { $sort: { name: 1 }}
-                ]
-            }})
-            model.aggregate(aggregation)
-            .then(data => {
-                res.json(data)
-            })
-            .catch(err => {
-                res.status(408).json({ error: err.message })
-            })
+      try {
+        let status = JSON.parse(req.query.status);
+        if (Array.isArray(status)) {
+          aggregation.push({ $match: { status: { $in: status } } });
         }
-    },
-
-    post: (req, res) => {
-        const instance = new model(req.body)
-        instance.save()
-        .then(data => {
-            res.json(data)
+      } catch (err) {}
+      aggregation.push({ $skip: parseInt(req.query.skip) || 0 });
+      aggregation.push({ $limit: parseInt(req.query.limit) || 10 });
+      model
+        .aggregate(aggregation)
+        .then((data) => {
+          res.json(data);
         })
-        .catch(err => {
-            res.status(406).json({ error: err.message })
-        })    
-    },
-
-    put: (req, res) => {
-        const _id = req.query._id
-        model.findOneAndUpdate({ _id }, req.body, { new: true, runValidators: true })
-        .then(updated => {
-            if(updated) {
-                res.json(updated)
-            } else {
-                res.status(404).json({ error: 'No such object' })
-            }
-        })
-        .catch(err => {
-            res.status(406).json({ error: err.message })
-        })    
-    },
-
-    delete: (req, res) => {
-        const _id = req.query._id
-        model.findOneAndDelete({ _id }).then((deleted) => {
-            if(deleted) {
-                res.json(deleted)
-            } else {
-                res.status(404).json({ error: 'No such object' })
-            }
-        }).catch(err => {
-            res.status(400).json({ error: err.message })
-        })
+        .catch((err) => {
+          res.status(408).json({ error: err.message });
+        });
     }
+  },
 
-}
+  post: (req, res) => {
+    const instance = new model(req.body);
+    instance
+      .save()
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.status(406).json({ error: err.message });
+      });
+  },
+
+  put: (req, res) => {
+    const _id = req.query._id;
+    model
+      .findOneAndUpdate({ _id }, req.body, { new: true, runValidators: true })
+      .then((updated) => {
+        if (updated) {
+          res.json(updated);
+        } else {
+          res.status(404).json({ error: "No such object" });
+        }
+      })
+      .catch((err) => {
+        res.status(406).json({ error: err.message });
+      });
+  },
+
+  delete: (req, res) => {
+    const _id = req.query._id;
+    model
+      .findOneAndDelete({ _id })
+      .then((deleted) => {
+        if (deleted) {
+          person
+            .getModel()
+            .updateMany({}, { $pull: { projects: _id } })
+            .then(() => res.json(deleted))
+            .catch((err) => res.status(400).json({ error: err.message }));
+        } else {
+          res.status(404).json({ error: "No such object" });
+        }
+      })
+      .catch((err) => {
+        res.status(400).json({ error: err.message });
+      });
+  },
+};
